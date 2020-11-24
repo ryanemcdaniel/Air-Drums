04d07d151dae1c7fc3267c80ec2bf71cb6ceafeb
remcdaniel <remcdaniel@uiowa.edu>
2020-11-23 21:16:19 -0600

    generating hands

--- internal/test/hand_generator.cs
+++ internal/test/hand_generator.cs
@@ -3,40 +3,54 @@ using Leap;
 
 public class Hand_Generator{
 
-    private Data_Generator vg;
+    private Data_Generator dg;
 
     public Hand_Generator(){
-        vg = new Data_Generator();
-    }
-
-    public Bone newBone(){
-        return new Bone();
+        dg = new Data_Generator();
     }
 
     public Finger newFinger(){
-        Finger ret = new Finger(
-            vg.newLong(100),
-            vg.newInt(100),
-            vg.newInt(100),
-            vg.newFloat(100),
-            vg.newVector(),
-            vg.newVector(),
-            vg.newVector(),
-            vg.newVector(),
-            vg.newFloat(100),
-            vg.newFloat(100),
+        return new Finger(
+            dg.newLong(100),
+            dg.newInt(100),
+            dg.newInt(100),
+            dg.newFloat(100),
+            dg.newVector(),
+            dg.newVector(),
+            dg.newVector(),
+            dg.newVector(),
+            dg.newFloat(100),
+            dg.newFloat(100),
             false,
             Leap.Finger.FingerType.TYPE_INDEX,
-            newBone(),
-            newBone(),
-            newBone(),
-            newBone()
+            new Bone(),
+            new Bone(),
+            new Bone(),
+            new Bone()
         );
-        return ret;
     }
 
     public Hand newHand(){
-        Hand ret = new Hand();
-        return ret;
+        return new Hand{
+            FrameId = dg.newLong(100),
+            Id = dg.newInt(100),
+            Confidence = dg.newFloat(100),
+            GrabStrength = dg.newFloat(100),
+            GrabAngle = dg.newFloat(100),
+            PinchStrength = dg.newFloat(100),
+            PinchDistance = dg.newFloat(100),
+            PalmWidth = dg.newFloat(100),
+            IsLeft = false,
+            TimeVisible = dg.newFloat(100),
+            Arm = null,
+            Fingers = null,
+            PalmPosition = dg.newVector(),
+            StabilizedPalmPosition = dg.newVector(),
+            PalmVelocity = dg.newVector(),
+            PalmNormal = dg.newVector(),
+            Rotation = new LeapQuaternion(),
+            Direction = dg.newVector(),
+            WristPosition = dg.newVector()
+        };
     }
 }
