aad4bc47c3cbe0e96076cdb50e625bc4d793153b
remcdaniel <remcdaniel@uiowa.edu>
2020-11-27 19:57:05 -0600

    Fixed stale entry point

--- internal/helpers/jointsHelper_generics.cs
+++ internal/helpers/jointsHelper_generics.cs
@@ -1,11 +1,11 @@
 using Leap;
 
-public delegate Vector   Apply_Vectors      (Vector v1      , Vector v2     );
-public delegate Vector   Scale_Vectors      (Vector v       , float f       );
-public delegate Vector[] Apply_VectorLists  (Vector[] vL1   , Vector[] vL2  );
-public delegate Vector[] Scale_VectorLists  (Vector[] vL    , float f       );
-public delegate Joints   Apply_Joints       (Joints j1      , Joints j2     );
-public delegate Joints   Scale_Joints       (Joints j       , float f       );
+public delegate Vector   Apply_Vectors      (Vector v1   , Vector v2   );
+public delegate Vector   Scale_Vectors      (Vector v    , float f     );
+public delegate Vector[] Apply_VectorLists  (Vector[] vL1, Vector[] vL2);
+public delegate Vector[] Scale_VectorLists  (Vector[] vL , float f     );
+public delegate Joints   Apply_Joints       (Joints j1   , Joints j2   );
+public delegate Joints   Scale_Joints       (Joints j    , float f     );
 
 public interface IJointsHelper {
 
