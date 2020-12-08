49530b1b40077ec6f6e2899e8d704c95e93ebc25
remcdaniel <remcdaniel@uiowa.edu>
2020-12-08 13:57:48 -0600

    beginning making local lookback variables

--- internal/gesture/classify.cs
+++ internal/gesture/classify.cs
@@ -9,23 +9,26 @@ public class Classify : IClassify {
     private IStats s;
     private List<Joints> pos;
     private List<Joints> vel;
+    private int lookback;
 
     public Classify(IVectorHelper vecH, IStats stats) {
         vh = vecH;
         s = stats;
+        lookback = GBL.N_LOOKBACK;
     }
 
     public bool IsMovement(List<Joints> positions) {
+        
+        // Do not classify movements until enough samples have loaded
         if(positions.Count != GBL.N_SAMPLES) return false;
-        var flag = false;
+
+        // Small position change in detection window rejection
         var range = s.range(positions);
         foreach (var v in range.ToArray()){
             var checks = vh.greaterEqual(v, GBL.NO_GESTURE_RANGE);
-            if (checks.x || checks.y || checks.z) flag = true;
+            if (checks.x || checks.y || checks.z) return true;
         }
-    
-
-        return flag;
+        return false;
     }
 
     public bool IsTap(List<Joints> pos, List<Joints> vel) {
@@ -57,28 +60,25 @@ public class Classify : IClassify {
             // Find lookback values
             var lookback = vel.GetRange(GBL.LookbackStart(), GBL.N_LOOKBACK);
             
-            // Recent velocity magnitude criteria
+            // Low velocity 
             var velAve = vh.average(s.average(lookback).TipsNoThumb()).y;
             if (velAve > -100) {
                 GBL.LookbackReset();
                 return false;
             }
 
+            // Reject 
             var finalVelRange = vh.average(s.range(lookback).TipsNoThumb()).y;
             if (finalVelRange > 100.0f) {
                 GBL.LookbackReset();
                 return false;
             }
 
-
-
-            
             // Accept moment of deceleration
             GBL.LookbackReset();
             return true;
 
         };
-
         return false;
     }
 
