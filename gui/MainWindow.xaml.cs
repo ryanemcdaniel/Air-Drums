f0632b0b1c44c40693475647f7200d08dc697745
gmilroy <grace-milroy@uiowa.edu>
2020-12-13 14:12:15 -0800

    added event handling

--- gui/MainWindow.xaml.cs
+++ gui/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System;
+using Global;
+
+
+
 
 namespace gui
 {
@@ -23,7 +17,51 @@ namespace gui
         public MainWindow()
         {
             InitializeComponent();
-            noteOffTime.Text = "centi-sec";
+            CMD commands= new CMD();
+            NoteOffTime.Text = "centi-sec";
+        }
+
+        private void KeyHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+            
+                if(sender == command1 || sender == data1_1 || sender == data2_1)
+                {
+                
+                
+                
+                }
+                else if(sender == command2 || sender == data1_2 || sender == data2_2)
+                {
+
+                }
+                else if(sender == command3 || sender == data1_3 || sender == data2_3)
+                {
+
+                }
+                else if(sender == command4 || sender == data1_4 || sender == data2_4)
+                {
+
+                }
+                else if(sender == command5 || sender == data1_5 || sender == data2_5)
+                {
+
+                }
+                else if(sender == command6 || sender == data1_6 || sender == data2_6)
+                {
+
+                }
+                else if(sender == command7 || sender == data1_7 || sender == data2_7)
+                {
+
+                }
+                else if(sender == command8 || sender == data1_8 || sender == data2_8)
+                {
+
+                }
+                
+            }
         }
     }
 }
