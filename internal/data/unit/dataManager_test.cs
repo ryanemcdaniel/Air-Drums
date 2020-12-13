75a80b898f73846614cf376754b5dcc5013aff22
jrcorcoran <jrcorcoran@uiowa.edu>
2020-12-13 14:54:09 -0600

    Extract_ClearsQueues Passing in dataManager_test

--- internal/data/unit/dataManager_test.cs
+++ internal/data/unit/dataManager_test.cs
@@ -1,6 +1,7 @@
 using Xunit;
 using Moq;
 using Leap;
+using System.Collections.Generic;
 
 public class dataManager_test {
     
@@ -43,10 +44,26 @@ public class dataManager_test {
 
         mock_l.Verify(m => m.LoadSample(dat_f1.Hands[1], dat_f1.CurrentFramesPerSecond), Times.Once());
         mock_2.Verify(m => m.LoadSample(dat_f2.Hands[1], dat_f2.CurrentFramesPerSecond), Times.Once());
-
     }
 
     [Fact] public void Extract_ClearsQueues() {
-        Assert.True(false);
+        Data_Generator dg = new Data_Generator();
+        Hand_Generator hg = new Hand_Generator(dg);
+        var dat_f = hg.newFrame();
+        dat_f.Hands[0].IsLeft = false;
+        dat_f.Hands[1].IsLeft = true;
+
+        var mock_l = new Mock<IQueues>();
+        mock_l.Setup(m => m.LoadSample(dat_f.Hands[1], dat_f.CurrentFramesPerSecond));
+
+        var dm = new DataManager(mock_l.Object, true);
+        dm.Extract(dat_f);
+
+        var dataCount = dm.GetData();
+        (List<Joints> position, List<Joints> velocity) exp;
+        exp.position = null;
+        exp.velocity = null;
+
+        test.Equals(dataCount, exp);
     }
 }
